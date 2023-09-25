1281673b49dcb80fd9a8c6f2bfbca993df1d2a58
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2023-09-25 23:43:01 +0530

    Time: 20 ms (45.09%) | Memory: 11.3 MB (21.23%) - LeetSync

--- SQL/Easy/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ SQL/Easy/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<int> mp;
+        unordered_set<char> mp;
         int l=0;
         int res=0;
         for(int r=0;r<s.length();r++){
