3dcd1f1d334ddf0f64b65a8a58b4adf4c3acb297
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2023-09-28 23:30:09 +0530

    Time: 25 ms (88.54%) | Memory: 18.8 MB (14.86%) - LeetSync

---
+++ SQL/Easy/143-reorder-list/reorder-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+      stack<ListNode*> st;
+      ListNode *curr=head;
+      while(curr!=NULL){
+          st.push(curr);
+          curr=curr->next;
+      }
+      curr=head;
+      ListNode *next;
+      int n=st.size();
+      for(int i=0;i<n/2;i++){
+        next=curr->next;
+        curr->next=st.top();
+        st.pop();
+        curr=curr->next;
+        curr->next=next;
+        curr=curr->next;        
+      }
+      curr->next=NULL;
+
+      
+    }
+};
