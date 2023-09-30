df30d2d766de9cf6d9c241d8a2a3e7bfdc7cc2ee
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2023-09-30 21:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (6.70%) - LeetSync

--- SQL/Easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ SQL/Easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         int ans=-1;
-        for(int i=0;i<haystack.size();i++){
-            ans=haystack.find(needle);
+        
+        ans=haystack.find(needle);
 
-        }
 
         return ans;
 
