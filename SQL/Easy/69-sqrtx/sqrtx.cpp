7a2872987c8ff069df138c0847a8e9b043ddd713
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2023-10-02 00:05:35 +0530

    Time: 23 ms (5.54%) | Memory: 6.2 MB (18.13%) - LeetSync

--- SQL/Easy/69-sqrtx/sqrtx.cpp
+++ SQL/Easy/69-sqrtx/sqrtx.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        int l=1,hi=x;
-        int res=0;
-        while(l<=hi){
-            int mid=l+(hi-l)/2;
-            if(pow(mid,2)==x){
-                return mid;
-            }
-            else if(pow(mid,2)>x){
-                hi=mid-1;
-            }else{
-                l=mid+1;
-                res=mid;
-            }
+        int i;
+        for(i=1;i<x;i++){
+            if(pow(i,2)>x)
+                break;
         }
-        return res;
+        if(x==1)
+            return 1;
+        else
+            return i-1;
     }
 };
