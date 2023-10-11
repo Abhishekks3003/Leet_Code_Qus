001dfda7777919b19a0b8a2736804f9ad562e84a
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2023-10-12 01:32:47 +0530

    Time: 12 ms (88.04%) | Memory: 24.1 MB (7.03%) - LeetSync

--- SQL/Easy/344-reverse-string/reverse-string.cpp
+++ SQL/Easy/344-reverse-string/reverse-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i=0;
-        int j=s.size()-1;
+        
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            st.push(s[i]);
+        }
 
-        while(i<j){
-            swap(s[i],s[j]);
-            i++;
-            j--;
+        for(int i=0;i<s.size();i++){
+            s[i]=st.top();
+            st.pop();
         }
     }
 };
