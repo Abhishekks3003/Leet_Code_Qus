fcba74589717725ad9d9cc759059241a6787ea63
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2024-03-05 12:38:14 +0530

    Time: 13 ms (25.16%) | Memory: 8.6 MB (44.12%) - LeetSync

--- SQL/Easy/242-valid-anagram/valid-anagram.cpp
+++ SQL/Easy/242-valid-anagram/valid-anagram.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        if(s.length()!=t.length()){
-            return 0;
-        }
-        map<char,int> mp;
-        for(int i=0;i<s.length();i++)
-            mp[s[i]]++;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
 
 
-        for(int i=0;i<t.length();i++){
-            if(mp.find(t[i])!=mp.end()){
-                if(mp[t[i]]>0)
-                    mp[t[i]]--;
-                else
-                    return 0;    
-            }else{
-                return 0;
-            }
-        }   
+        if(s==t)
+            return 1;
 
-        return 1;
+        return 0;    
     }
 };
