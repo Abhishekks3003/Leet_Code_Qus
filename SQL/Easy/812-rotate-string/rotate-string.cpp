55cbf5f08a63b90ccc6fa8f75f9d29cd57c269ad
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2024-03-12 18:25:33 +0530

    Time: 5 ms (10.00%) | Memory: 8.5 MB (13.18%) - LeetSync

---
+++ SQL/Easy/812-rotate-string/rotate-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        string s1;
+
+        int i=0;
+        int n=s.length();
+        while(i++<n){
+            s1=s.substr(1,n-1);
+            s1+=s[0];
+
+            if(s1==goal){
+                return 1;
+                break;
+            }
+            else{   
+                s=s1;
+                s1="";
+            }
+        }
+        return 0;    
+    }
+};
