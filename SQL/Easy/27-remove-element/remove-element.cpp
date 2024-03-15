9f2eb803f3ac4275bcbf38b8920dfefce0382d51
Abhishek Kumar Singh <75007991+Abhishekks3003@users.noreply.github.com>
2024-03-15 10:41:07 +0530

    Time: 6 ms (26.57%) | Memory: 10.7 MB (35.29%) - LeetSync

--- SQL/Easy/27-remove-element/remove-element.cpp
+++ SQL/Easy/27-remove-element/remove-element.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-        auto it=nums.begin();
-        while(it!=nums.end()){
-            if(*it==val){
-                nums.erase(it);
-            }else{
-                it++;
+        multiset<int> ms;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                ms.insert(nums[i]);
             }
         }
 
-        return nums.size();
+        // int j=0;
+        // for(auto a:s){
+        //     nums[j++]=a;
+        // 
+        // for(int i=0;i<mp.size();i++){
+
+        // }
+
+        int j=0;
+
+        for(auto a:ms)
+            nums[j++]=a;
+
+        return ms.size();
     }
 };
